bfe45922ef8220aabca547ad194854a11e0e725c
Ehtesham Zulfiqar <ehtesham@renorun.ca>
2022-11-11 20:23:14 -0500

    feat(container): made the container responsive

--- src/components/Container/Container.styles.ts
+++ src/components/Container/Container.styles.ts
@@ -1,8 +1,21 @@
 import styled from "styled-components";
 
 export const StyledContainer = styled.div`
-  width: 1024px;
+  width: 1200px;
   margin: 20px auto;
+
+  @media (max-width: 1240px) {
+    width: 900px;
+  }
+
+  @media (max-width: 940px) {
+    width: 600px;
+  }
+
+  @media (max-width: 640px) {
+    width: calc(100% - 40px);
+    margin: 20px;
+  }
 `;
 
 type StyledWrapperPropsType = {
@@ -13,4 +26,10 @@ export const StyledWrapper = styled.div<StyledWrapperPropsType>`
   display: flex;
   column-gap: 32px;
   align-items: ${(props) => (props.isInitiated ? "flex-start" : "initial")};
+
+  @media (max-width: 940px) {
+    flex-direction: column;
+    row-gap: 32px;
+    align-items: initial;
+  }
 `;
